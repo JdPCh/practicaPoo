902c5979983089479230969b8f25a36254fe70c3
Jesús David Peña Chivatá <jdpena.chivata@unillanos.edu.co>
2025-02-18 11:51:43 -0500

    Test PracticaPOO.java

--- src/main/java/tecnologiasavanzadas/practicapoo/PracticaPOO.java
+++ src/main/java/tecnologiasavanzadas/practicapoo/PracticaPOO.java
@@ -11,8 +11,16 @@ package tecnologiasavanzadas.practicapoo;
 public class PracticaPOO {
 
     public static void main(String[] args) {
-        Programa programa = new Programa();
-        Estudiante estudiante = new Estudiante(1.0, programa, true, 5.0 ,1.0, "pepe", "lozano", "123");
+        Date fecha = new Date();
+        
+        Persona persona = new Persona(5.0, "Jorge", "Sanchez", "JSan@universidad.com");
+        
+        Facultad facultad = new Facultad(4.0,"FCBI", persona);
+        
+        Programa programa = new Programa(2.0, "Sistemas", 6.0, fecha, facultad);
+        
+        
+        Estudiante estudiante = new Estudiante(1.0, programa, true, 5.0 ,1.0, "pepe", "lozano", "pepe@universidad.com");
         
         
         
