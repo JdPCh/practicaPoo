5f9d6ea634f54d38d294fd0bdbb031a6a12722ce
Jesús David Peña Chivatá <jdpena.chivata@unillanos.edu.co>
2025-02-23 18:23:35 -0500

    Update InscripcionesPersonas.java

--- src/main/java/tecnologiasavanzadas/practicapoo/InscripcionesPersonas.java
+++ src/main/java/tecnologiasavanzadas/practicapoo/InscripcionesPersonas.java
@@ -4,6 +4,11 @@
  */
 package tecnologiasavanzadas.practicapoo;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -15,8 +20,17 @@ public class InscripcionesPersonas {
     private
     
     ArrayList<Persona> listado;
-    
+
     public
+    
+    @Override
+    String toString(){
+        String resultado = "Personas Inscritas: \n";
+        for(Persona persona : listado){
+            resultado += persona + "\n\n";
+        }
+        return resultado;
+    }
     
     InscripcionesPersonas() {
         this.listado = new ArrayList();
@@ -34,9 +48,58 @@ public class InscripcionesPersonas {
         this.listado = listado;
     }
     
-    void inscribir(Persona persona){};
-    void eliminar(Persona persona){};
-    void actualizar(Persona persona){};
-    void guardarInformacion(Persona persona){};
-    void cargarDatos(){};
+    void inscribir(Persona persona){
+        listado.add(persona);
+        System.out.println("Persona inscrita correctamente");
+    };
+    void eliminar(Persona persona){
+        listado.remove(persona);
+        System.out.println("Persona eliminada correctamente");
+    };
+    void actualizar(Persona persona){
+        
+        
+    };
+    void guardarInformacion(Persona persona){
+        try{
+            BufferedWriter escritor = new BufferedWriter(new FileWriter("src/main/java/OtherResources/Personas.txt", true));
+            escritor.write("Id: " + persona.getId() + ", ");
+            escritor.write("Nombre: " + persona.getNombre() + ", ");
+            escritor.write("Apellido: " + persona.getApellido() + ", ");
+            escritor.write("Email: " + persona.getEmail());
+            escritor.newLine();
+            escritor.flush();
+            System.out.println("Persona guardada correctamente");
+        }
+        catch(IOException e){
+            System.out.println("Ocurrió un error al escribir el archivo: " + e.getMessage());
+        }
+    };
+    void cargarDatos(){
+
+        try (BufferedReader lector = new BufferedReader(new FileReader("src/main/java/OtherResources/Personas.txt"))) {
+            String linea;
+            while ((linea = lector.readLine()) != null) { 
+                 String[] partes = linea.split(", ");
+                 
+                 String id = partes[0].split(": ")[1]; 
+                 String nombre = partes[1].split(": ")[1]; 
+                 String apellido = partes[2].split(": ")[1]; 
+                 String email = partes[3].split(": ")[1]; 
+                 
+                 Persona persona = new Persona();
+                 
+                 persona.setId(Double.valueOf(id));
+                 persona.setNombre(nombre);
+                 persona.setApellido(apellido);
+                 persona.setEmail(email);
+                 
+                 listado.add(persona);
+            }
+            System.out.println("Personas cargadas con exito: ");
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+    
+    };
 }
