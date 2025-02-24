65510c0cb5ea28b17f82b33c130a0bd5267b0a47
Morbius131 <125073592+Morbius131@users.noreply.github.com>
2025-02-23 21:47:01 -0500

    Update PracticaPOO.java

--- src/main/java/tecnologiasavanzadas/practicapoo/PracticaPOO.java
+++ src/main/java/tecnologiasavanzadas/practicapoo/PracticaPOO.java
@@ -4,6 +4,10 @@
 
 package tecnologiasavanzadas.practicapoo;
 
+
+import java.util.Date;
+
+
 /**
  *
  * @author Estudiante_MCA
@@ -11,6 +15,8 @@ package tecnologiasavanzadas.practicapoo;
 public class PracticaPOO {
 
     public static void main(String[] args) {
+
+
         Date fecha = new Date();
         
         Persona persona = new Persona(5.0, "Jorge", "Sanchez", "JSan@universidad.com");
@@ -19,10 +25,48 @@ public class PracticaPOO {
         
         Programa programa = new Programa(2.0, "Sistemas", 6.0, fecha, facultad);
         
-        
+
         Estudiante estudiante = new Estudiante(1.0, programa, true, 5.0 ,1.0, "pepe", "lozano", "pepe@universidad.com");
         
         
+        Profesor profesor = new Profesor(6.0, "Carlos", "Pulido", "CarlosP@universidad.com","tiempoCompleto");
+        
+        Curso curso = new Curso(1, "Matematicas", programa, true);
+        
+        CursoProfesor cursoProfesor = new CursoProfesor(profesor, 2025, 1, curso);
+        
+        
+        InscripcionesPersonas inscripcionesPersonas = new InscripcionesPersonas();
+        
+        CursosProfesores cursosProfesores = new CursosProfesores();
+        
+        CursosInscritos cursosInscritos = new CursosInscritos();
+        
+        Inscripcion inscripcion = new Inscripcion(curso, 2025, 1, estudiante);
+        
+        inscripcionesPersonas.inscribir(persona);
+        
+        inscripcionesPersonas.guardarInformacion(estudiante);
+        
+        inscripcionesPersonas.cargarDatos();
+        
+        inscripcionesPersonas.eliminar(persona);
+        
+        cursosProfesores.inscribir(cursoProfesor);
+        
+        cursosProfesores.guardarInformacion(cursoProfesor);
+        
+        cursosProfesores.cargarDatos();
+        
+        cursosInscritos.guardarInformacion(inscripcion);
+        
+        cursosInscritos.cargarDatos();
+        
+      
+        
+        System.out.println(inscripcionesPersonas);
+        System.out.println(cursosProfesores);
+        System.out.println(cursosInscritos);
         
         System.out.println(estudiante.toString());
     }
